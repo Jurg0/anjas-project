e80e98f4a0a97e2f39ef9167c8a4d60e85b6cc15
Jurg0 <41382724+Jurg0@users.noreply.github.com>
2018-11-20 11:34:27 +0100

    Add files via upload

--- ap.R
+++ ap.R
@@ -1,5 +1,19 @@
 library(dplyr)
 library(reshape2)
+#library(ggplot2)
 
-a <- read.csv("dat.csv", header = TRUE, sep = ";")
+# Daten einlesen und umformen
+a <- read.csv("dat.csv", header = TRUE, sep = ";") # Einlesen von meinem Beispieldatensatz, muss ggf. noch auf Excel-Datei angepasst werden
 b <- melt(a, id.vars="Experiment")
+
+x <- rep(c(1,2,3,4), each = 96) %>% # Kreiert einen Vektor als Index für die Samples, je 96 Datenpunkte
+  rep(times = 20) # Die Zahl muss angepasst werden an die Anzahl deiner Zeilen im Datensatz
+b <- mutate(b, sample = x) # Fügt dem Datensatz den Sample Index hinzu
+
+# Plotting funktioniert noch nicht so gut ^^'
+# experiment1 <- subset(b, Experiment == 1) 
+# ggplot(experiment1) +
+#   geom_quasirandom(aes(x = variable, y = value)) +
+#   #facet_wrap(~ variable) +
+#   xlab("well") +
+#   ylab("value")
